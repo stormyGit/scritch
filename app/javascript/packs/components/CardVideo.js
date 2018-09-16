a65c7b70ad5911b2ac087a3b46b098f4c28cea11
d2458a65fb <contact@howlr.im>
2018-09-16 13:24:47 +0200

    better video player

--- app/javascript/packs/components/CardVideo.js
+++ app/javascript/packs/components/CardVideo.js
@@ -3,43 +3,49 @@ import Avatar from '@material-ui/core/Avatar';
 import CardMedia from '@material-ui/core/CardMedia';
 import Hls from 'hls.js';
 
+import 'mediaelement';
+import 'mediaelement/build/mediaelementplayer.min.css';
+
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   video: {
-    width: 'calc(100% - 400px)',
+    height: '100%',
+    marginLeft: "auto",
+    marginRight: "auto",
   },
-  media: {
-    minHeight: 'calc(100vh - 128px)',
+  container: {
+    width: "100%",
+    background: "black",
+    height: "80vh",
   },
 });
 
 class CardVideo extends React.Component {
   componentDidMount() {
-    if (Hls.isSupported()) {
-      this.hls = new Hls();
-      this.hls.loadSource(`${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${this.props.medium.key}`);
-      this.hls.attachMedia(this.refs.video);
-      this.hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        this.refs.video.play();
-      });
+    window.Hls = Hls;
+
+    this.setState({
+      player: new MediaElementPlayer(this.refs.video, {})
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.state.player) {
+      this.state.player.remove();
+      this.setState({player: null});
     }
   }
 
   render() {
     const { medium, classes } = this.props;
-
+    console.log(this.state);
     return (
-      <video ref="video" className={classes.video} />
-    )
-
-    return (
-      <CardMedia
-        className={classes.media}
-        image={`https://placeimg.com/640/480/${medium.id}`}
-        title={medium.title}
-      >
-      </CardMedia>
+      <div className={classes.container}>
+        <video ref="video" className={classes.video} height="100%">
+          <source src={`${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${this.props.medium.key}`} />
+        </video>
+      </div>
     );
   }
 }
