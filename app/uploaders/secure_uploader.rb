731381b98587eaf3606c88632259d7ca6b13cfcc
d2458a65fb <contact@howlr.im>
2018-10-13 17:28:56 +0200

    simplify uploader

--- app/uploaders/secure_uploader.rb
+++ app/uploaders/secure_uploader.rb
@@ -6,12 +6,6 @@ class SecureUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-  def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    "picture.#{file.extension}" if original_filename.present?
   end
 end
