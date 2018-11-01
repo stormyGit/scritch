31890a20fa68bf18e545f6f51a8fe78a70db4d5c
d2458a65fb <contact@howlr.im>
2018-11-01 21:31:55 +0100

    add sitemap stuff

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,2 +1,9 @@
 Sitemap::Generator.instance.load :host => ENV["DOMAIN"] do
+  Medium.published.publicly_available.find_each do |medium|
+    literal "/videos/#{medium.slug}-#{medium.uuid}"
+  end
+
+  User.where(public: true).where(uuid: Medium.published.publicly_available.select(:user_id)).find_each do |user|
+    literal "/#{user.slug}"
+  end
 end
