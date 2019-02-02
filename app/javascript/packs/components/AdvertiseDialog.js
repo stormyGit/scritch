0f14f2f32a5e43c479fb11bc2879d83157eef2ea
stormy <raphael.beltrand@gmail.com>
2019-02-02 22:58:44 +0100

    its panic time

--- app/javascript/packs/components/AdvertiseDialog.js
+++ app/javascript/packs/components/AdvertiseDialog.js
@@ -9,6 +9,12 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import CheckIcon from "@material-ui/icons/Check";
 import { Link, withRouter } from "react-router-dom";
 
 import { withStyles } from "@material-ui/core/styles";
@@ -18,7 +24,11 @@ import withCurrentSession from "./withCurrentSession";
 
 import {} from "../queries";
 
-const styles = theme => ({});
+const styles = theme => ({
+  content: {
+    fontWeight: 200
+  }
+});
 
 class AdvertiseDialog extends React.Component {
   state = {
@@ -54,8 +64,49 @@ class AdvertiseDialog extends React.Component {
       >
         <GlobalProgress absolute />
 
-        <DialogTitle>Advertise on Scritch!</DialogTitle>
+        <DialogTitle>Advertise with Scritch!</DialogTitle>
         <DialogContent>
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="h4"
+            className={classes.content}
+          >
+            You want a bunch of furries to see what you have to offer? Put up an
+            ad on Scritch!
+            <br />
+            <br />
+            We offer 4 advertisement slots, visible on every page. 2 at the top,
+            2 at the bottom.
+            <br />
+            <br />
+            Following are the requirements for the ad file. If you have a 300x90
+            ad on FA going on, you should already be set to go without changing
+            the file!
+          </Typography>
+          <br />
+          <br />
+          <List>
+            <ListItem>
+              <ListItemIcon>
+                <CheckIcon />
+              </ListItemIcon>
+              <ListItemText
+                inset
+                primary="300x90 image ratio (smaller may not look smooth, bigger is fine up to 900x270)"
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <CheckIcon />
+              </ListItemIcon>
+              <ListItemText
+                inset
+                primary="Image type should be JPEG, PNG, or GIF"
+              />
+            </ListItem>
+          </List>
+
           <TextField
             label="stuff"
             name="description"
