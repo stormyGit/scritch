8cf68a511b8ddf0e92ad0bf6fa0c336cc4d62b70
Stormy <raphael.beltrand@gmail.com>
2019-02-07 20:54:03 -0800

    payment processing close to done

--- app/jobs/cancel_sponsorship.rb
+++ app/jobs/cancel_sponsorship.rb
@@ -2,9 +2,9 @@ class CancelSponsorship < ApplicationJob
   queue_as :default
 
   def perform()
-    Sponsor.where(status: "canceled", limit < Time.now).each do |e|
-      e.destroy
-    end
+    #Sponsor.where(status: "canceled", limit: < Time.now).each do |e|
+    #  e.destroy
+    #end
   end
 
 end
