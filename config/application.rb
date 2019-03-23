3741766c61a9176a878187036679f09c9cd314d9
Stormy <raphael.beltrand@gmail.com>
2019-03-22 18:34:56 -0700

    fix app rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Scritch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.web_console.whitelisted_ips = "54.187.174.169"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
