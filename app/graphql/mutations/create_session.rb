e8ee249229e1ed9c6a63638d6362f5fdb135e43e
Stormy <raphael.beltrand@gmail.com>
2019-04-23 00:20:35 -0700

    telegram login

--- app/graphql/mutations/create_session.rb
+++ app/graphql/mutations/create_session.rb
@@ -13,10 +13,10 @@ class Mutations::CreateSession < Mutations::BaseMutation
   def resolve(params)
     telegram_hash = params.delete :telegram_hash
     check_string = params.map { |k, v| "#{k.to_s.gsub(/^telegram_/, '')}=#{v}" }.sort.join("\n")
-    # if OpenSSL::HMAC.hexdigest("SHA256", Digest::SHA256.digest(Telegram.bot.token), check_string) != telegram_hash
-    #   ExceptionNotifier.notify_exception Exception.new("Telegram hash mismatch: #{telegram_hash}: #{params.to_json}")
-    #   raise Pundit::NotAuthorizedError
-    # end
+    if OpenSSL::HMAC.hexdigest("SHA256", Digest::SHA256.digest(Telegram.bots[:login].token), check_string) != telegram_hash
+      ExceptionNotifier.notify_exception Exception.new("Telegram hash mismatch: #{telegram_hash}: #{params.to_json}")
+      raise Pundit::NotAuthorizedError
+    end
 
     user = User.find_or_create_by(telegram_id: params[:telegram_id]) do |user|
       user.telegram_id = params[:telegram_id]
@@ -28,22 +28,22 @@ class Mutations::CreateSession < Mutations::BaseMutation
         user.name = params[:telegram_first_name]
       end
 
-      # begin
-      #   profile_photos = Telegram.bot.get_user_profile_photos(user_id: params[:telegram_id], limit: 1)
-      #   return if profile_photos["result"]["total_count"] == 0
-      #
-      #   profile_photo_file = profile_photos["result"]["photos"][0].sort_by do |profile_photo|
-      #     profile_photo["width"]
-      #   end.last
-      #   if profile_photo_file.present?
-      #     profile_photo_file_path = Telegram.bot.get_file(file_id: profile_photo_file['file_id'])["result"]["file_path"]
-      #
-      #     user.remote_avatar_url = "https://api.telegram.org/file/bot#{Telegram.bot.token}/#{profile_photo_file_path}"
-      #   end
-      #
-      # rescue => error
-      #   ExceptionNotifier.notify_exception(error)
-      # end
+      begin
+        profile_photos = Telegram.bots[:login].get_user_profile_photos(user_id: params[:telegram_id], limit: 1)
+        return if profile_photos["result"]["total_count"] == 0
+
+        profile_photo_file = profile_photos["result"]["photos"][0].sort_by do |profile_photo|
+          profile_photo["width"]
+        end.last
+        if profile_photo_file.present?
+          profile_photo_file_path = Telegram.bots[:login].get_file(file_id: profile_photo_file['file_id'])["result"]["file_path"]
+
+          user.remote_avatar_url = "https://api.telegram.org/file/bot#{Telegram.bots[:login].token}/#{profile_photo_file_path}"
+        end
+
+      rescue => error
+        ExceptionNotifier.notify_exception(error)
+      end
 
     end
 
