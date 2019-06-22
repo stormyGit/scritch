bb30be481ca63cd9e79729f7dcae4270d2a77b11
Stormy <raphael.beltrand@gmail.com>
2019-06-22 01:49:02 -0700

    edition back office hotfix

--- app/controllers/moderation/editions_controller.rb
+++ app/controllers/moderation/editions_controller.rb
@@ -37,7 +37,7 @@ class Moderation::EditionsController < ModerationController
     ]))
     #    authorize edition
     edition.year = edition.start_date.year
-    edition.guest_of_honours = params[:goh].split(", ")
+    edition.guest_of_honours = params[:goh]&.split(", ")
     edition.save!
     flash[:notice] = "Edition updated!"
     flash[:class] = "has-text-warning"
@@ -74,7 +74,7 @@ class Moderation::EditionsController < ModerationController
     ]))
     #    authorize edition
     edition.year = edition.start_date.year
-    edition.guest_of_honours = params[:goh].split(", ")
+    edition.guest_of_honours = params[:goh]&.split(", ")
     if edition.event.avatar.blank?
       begin
         edition.event.avatar = File.open("app/assets/images/events/Scritch Event Thumbnail - #{edition.country}.png")
