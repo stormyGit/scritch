05e811a7a1af33d722ad19127beea70cededed53
Stormy Main <raphael.beltrand@gmail.com>
2019-10-10 23:20:51 +0200

    update backup scripts

--- backup.sh
+++ backup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-export OLDER_STORAGE="/mnt/g/scritch_storage_backups/now"
-rsync -av -e ssh --no-o --delete  root@scritch.es:/var/lib/dokku/data/storage/minio/ "/mnt/g/scritch_storage_backups/now"
-#find /mnt/g/scritch_storage_backups/* -type d -ctime +7 -exec rm -rf {} +
-
-#rsync -av -e ssh --no-o --delete root@scritch.es:/root/scritch_db_backups/scritch_backup20190622_041318.dump "/mnt/g/scritch_storage_backups/"
+rsync -av -e ssh --no-o --delete  root@scritch.es:/var/lib/dokku/data/storage/minio/ "/media/stormy/Elements/scritch_storage_backups/now"
--- backup_db.sh
+++ backup_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -av -e ssh --no-o --delete root@scritch.es:/root/scritch_db_backups/* /mnt/g/scritch_db_backups/
+rsync -av -e ssh --no-o --delete root@scritch.es:/root/scritch_db_backups/* /media/stormy/Elements/scritch_db_backups/now
