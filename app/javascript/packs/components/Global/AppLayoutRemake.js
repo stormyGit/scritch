9b24fdca8a6e1afcdc798e9433aafa9e06b5c315
Stormy Main <raphael.beltrand@gmail.com>
2019-10-13 23:58:16 +0200

    SUPERFRESH

--- app/javascript/packs/components/Global/AppLayoutRemake.js
+++ app/javascript/packs/components/Global/AppLayoutRemake.js
@@ -39,6 +39,18 @@ const styles = theme => ({
     display: "flex",
     minHeight: "calc(100vh - 56px)"
   },
+  "@global": {
+    "*::-webkit-scrollbar": {
+      width: "0.4em"
+    },
+    "*::-webkit-scrollbar-track": {
+      "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.00)"
+    },
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(0,0,0,.1)",
+      outline: "1px solid slategrey"
+    }
+  },
   content: {
     flexGrow: 1,
     minWidth: 0, // So the Typography noWrap works
