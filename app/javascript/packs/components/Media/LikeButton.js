2313f62c4c7a769c96602685a1c6d7b56b1b7f1b
Stormy Main <raphael.beltrand@gmail.com>
2019-10-24 05:15:26 +0200

    scritch button color

--- app/javascript/packs/components/Media/LikeButton.js
+++ app/javascript/packs/components/Media/LikeButton.js
@@ -21,7 +21,7 @@ const NoFavoriteIcon = props => (
     <path fill="none" d="M0 0h24v24H0V0z" />
     <circle
       strokeWidth="2"
-      stroke={process.env.SECONDARY_COLOR}
+      stroke="#25E8AD"
       fill="none"
       cx="4.5"
       cy="9.5"
@@ -29,7 +29,7 @@ const NoFavoriteIcon = props => (
     />
     <circle
       strokeWidth="2"
-      stroke={process.env.SECONDARY_COLOR}
+      stroke="#25E8AD"
       fill="none"
       cx="9"
       cy="5.5"
@@ -37,7 +37,7 @@ const NoFavoriteIcon = props => (
     />
     <circle
       strokeWidth="2"
-      stroke={process.env.SECONDARY_COLOR}
+      stroke="#25E8AD"
       fill="none"
       cx="15"
       cy="5.5"
@@ -45,7 +45,7 @@ const NoFavoriteIcon = props => (
     />
     <circle
       strokeWidth="2"
-      stroke={process.env.SECONDARY_COLOR}
+      stroke="#25E8AD"
       fill="none"
       cx="19.5"
       cy="9.5"
@@ -53,7 +53,7 @@ const NoFavoriteIcon = props => (
     />
     <path
       strokeWidth="2"
-      stroke={process.env.SECONDARY_COLOR}
+      stroke="#25E8AD"
       fill="none"
       d="M17.34 14.86c-.87-1.02-1.6-1.89-2.48-2.91-.46-.54-1.05-1.08-1.75-1.32-.11-.04-.22-.07-.33-.09-.25-.04-.52-.04-.78-.04s-.53 0-.79.05c-.11.02-.22.05-.33.09-.7.24-1.28.78-1.75 1.32-.87 1.02-1.6 1.89-2.48 2.91-1.31 1.31-2.92 2.76-2.62 4.79.29 1.02 1.02 2.03 2.33 2.32.73.15 3.06-.44 5.54-.44h.18c2.48 0 4.81.58 5.54.44 1.31-.29 2.04-1.31 2.33-2.32.31-2.04-1.3-3.49-2.61-4.8z"
     />
