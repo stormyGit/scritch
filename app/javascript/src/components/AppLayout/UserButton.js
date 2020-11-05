9a6514a4e0160b05bd46f44d0ef866a79cf51f67
Sythelux Rikd <dersyth@gmail.com>
2020-11-05 20:33:20 +0000

    fix: updated UserButton to React 17

--- app/javascript/src/components/AppLayout/UserButton.js
+++ app/javascript/src/components/AppLayout/UserButton.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { Link, withRouter } from "react-router-dom";
+import React, {useContext, useState} from "react";
+import {withStyles} from "@material-ui/core/styles";
+import {Link, withRouter} from "react-router-dom";
 import withCurrentSession from "../withCurrentSession";
 import withWidth from "@material-ui/core/withWidth";
 import Button from "@material-ui/core/Button";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import { DELETE_SESSION } from "../../queries/globalQueries";
-import { Mutation } from "react-apollo";
+import {DELETE_SESSION} from "../../queries/globalQueries";
+import {Mutation} from "react-apollo";
 
 import UserAvatar from "../Users/UserAvatar";
+import {IconButton} from "@material-ui/core";
+import {DialogContext} from "../../context/DialogContext";
+import {setSettingsDialogState, setSignupDialogState} from "../../reducers/Action";
 
 const styles = theme => ({
   dataSpacer: {
@@ -18,86 +21,75 @@ const styles = theme => ({
   }
 });
 
-class UserButton extends React.Component {
-  state = {
-    uploadDialog: true
-  };
+function UserButton({classes, currentSession, width}) {
+  const dialogContext = useContext(DialogContext);
+  const [userMenuAnchor, setUserMenuAnchor] = useState(false);
 
-  render() {
-    const { classes, currentSession, width } = this.props;
-
-    return (
-      <React.Fragment>
-        {!currentSession && (
-          <Button
-            onClick={this.props.openSignUp}
-            variant="outlined"
-            size={width === "xl" || width === "lg" ? "large" : "small"}
+  return (
+    <React.Fragment>
+      {currentSession ? (
+        <React.Fragment>
+          <ButtonBase
+            focusRipple
+            className={classes.rightButton}
+            onClick={event => setUserMenuAnchor(event.currentTarget)}
           >
-            Sign Up/In
-          </Button>
-        )}
-        {currentSession && (
-          <React.Fragment>
-            <ButtonBase
-              focusRipple
-              className={classes.rightButton}
-              onClick={event =>
-                this.setState({
-                  userMenuAnchor: event.currentTarget
-                })
-              }
+            <UserAvatar user={currentSession.user}/>
+          </ButtonBase>
+          <Menu
+            id={`user-menu`}
+            anchorEl={userMenuAnchor}
+            open={Boolean(userMenuAnchor)}
+            onClose={() => setUserMenuAnchor(false)}
+            transformOrigin={{vertical: "top", horizontal: "center"}}
+          >
+            <MenuItem
+              component={props => <Link to={`/${currentSession.user.slug}`} {...props} />}
+              onClick={() => {
+                setUserMenuAnchor(false)
+              }}
             >
-              <UserAvatar user={currentSession.user} />
-            </ButtonBase>
-            <Menu
-              id={`user-menu`}
-              anchorEl={this.state.userMenuAnchor}
-              open={Boolean(this.state.userMenuAnchor)}
-              onClose={() => this.setState({ userMenuAnchor: null })}
-              transformOrigin={{ vertical: "top", horizontal: "center" }}
+              Profile
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setUserMenuAnchor(false);
+                dialogContext.dispatchDialogChange(setSettingsDialogState(true));
+              }}
             >
-              <MenuItem
-                component={props => <Link to={`/${currentSession.user.slug}`} {...props} />}
-                onClick={() => {
-                  this.setState({ userMenuAnchor: null });
-                }}
-              >
-                Profile
-              </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  this.setState({
-                    userMenuAnchor: null
-                  });
-                  this.props.openSettings();
-                }}
-              >
-                Settings and Security
-              </MenuItem>
+              Settings and Security
+            </MenuItem>
 
-              <Mutation mutation={DELETE_SESSION}>
-                {(deleteSession, { data }) => (
-                  <MenuItem
-                    onClick={() => {
-                      deleteSession({
-                        variables: { input: { id: currentSession.id } }
-                      }).then(() => {
-                        localStorage.setItem("token", null);
-                        location.reload();
-                      });
-                    }}
-                  >
-                    Logout
-                  </MenuItem>
-                )}
-              </Mutation>
-            </Menu>
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    );
-  }
+            <Mutation mutation={DELETE_SESSION}>
+              {(deleteSession, {data}) => (
+                <MenuItem
+                  onClick={() => {
+                    deleteSession({
+                      variables: {input: {id: currentSession.id}}
+                    }).then(() => {
+                      localStorage.setItem("token", null);
+                      location.reload();
+                    });
+                  }}
+                >
+                  Logout
+                </MenuItem>
+              )}
+            </Mutation>
+          </Menu>
+        </React.Fragment>
+      ) : (
+        <Button
+          color="inherit"
+          onClick={() => {dialogContext.dispatchDialogChange(setSignupDialogState(true));}}
+          variant="outlined"
+          // size={width === "xl" || width === "lg" ? "large" : "small"}
+        >
+          Sign Up/In
+        </Button>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default withStyles(styles)(withRouter(withCurrentSession(withWidth()(UserButton))));
